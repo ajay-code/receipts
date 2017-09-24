97ea855ebb19a937ce7bdf92a654462667ce6e63
Ajay Singh <ajay10mar96@gmail.com>
2017-09-24 13:06:57 +0530

    setting up for production

--- database/migrations/2017_06_29_064550_create_admin_password_resets_table.php
+++ database/migrations/2017_06_29_064550_create_admin_password_resets_table.php
@@ -14,7 +14,7 @@ class CreateAdminPasswordResetsTable extends Migration
     {
         Schema::create('admin_password_resets', function (Blueprint $table) {
             $table->string('username', 100)->index();
-            $table->string('token', 200)->index();
+            $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
